c2a74083f47ee4383f10a402d463e798b6590ac1
SThiviyan <63099414+SThiviyan@users.noreply.github.com>
2022-02-22 12:34:07 +0100

    StockDataFetch Files added

---
+++ ML_Lib/SharedDep/DataFetch/StockDataFetch.cpp
@@ -0,0 +1,8 @@
+//
+//  StockDataFetch.cpp
+//  ML_Lib
+//
+//  Created by Thiviyan Saravanamuthu on 27.12.21.
+//
+
+#include "StockDataFetch.hpp"
---
+++ ML_Lib/SharedDep/DataFetch/StockDataFetch.hpp
@@ -0,0 +1,27 @@
+//
+//  StockDataFetch.hpp
+//  ML_Lib
+//
+//  Created by Thiviyan Saravanamuthu on 27.12.21.
+//
+
+#ifndef StockDataFetch_hpp
+#define StockDataFetch_hpp
+
+#include <iostream>
+
+class StockDataFetch
+{
+public:
+    StockDataFetch();
+    
+    
+    
+private:
+    
+    
+    
+};
+
+
+#endif /* StockDataFetch_hpp */
