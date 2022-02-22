c1ab055c4158a21986ed35429e4fb01870f2fe47
SThiviyan <63099414+SThiviyan@users.noreply.github.com>
2022-02-22 12:40:02 +0100

    Added PoolingLayer Files

---
+++ ML_Lib/ConvNeuralNetwork/Headers/PoolingLayer.hpp
@@ -0,0 +1,27 @@
+//
+//  PoolingLayer.hpp
+//  ML_Lib
+//
+//  Created by Thiviyan Saravanamuthu on 07.08.21.
+//
+
+#ifndef PoolingLayer_hpp
+#define PoolingLayer_hpp
+
+#include <iostream>
+
+namespace ML_Lib {
+    
+    class Pool_Layer
+    {
+    public:
+        
+        
+    private:
+        
+        
+    };
+}
+
+
+#endif /* PoolingLayer_hpp */
---
+++ ML_Lib/ConvNeuralNetwork/Sources/PoolingLayer.cpp
@@ -0,0 +1,8 @@
+//
+//  PoolingLayer.cpp
+//  ML_Lib
+//
+//  Created by Thiviyan Saravanamuthu on 07.08.21.
+//
+
+#include "PoolingLayer.hpp"
